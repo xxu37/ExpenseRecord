97aff385a51c068c6609b9c73c34945c7d2b3109
Xinxin Xu <xxu37@slb.com>
2023-10-26 13:36:41 +0800

    first commit

---
+++ ExpenseRecord/Model/ExpenseRecordDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseRecord.Model
+{
+    public class ExpenseRecordDto
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required]
+        [StringLength(50)]
+        public string Description { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public int Amount { get; set; }
+        public string ExpenseDate {  get; set; } = string.Empty;
+    }
+}
